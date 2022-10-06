eff6d1fb85146d5e9b35200e4077d47e5b3cf74f
Eduardo Mauricio Gonçalves da Silva Manzano <e.goncalvesmanzano@gmail.com>
2022-10-06 15:35:08 -0300

    first commit

---
+++ concatenando e interpolando variáveis/scripts.js
@@ -0,0 +1,30 @@
+//Variáveis e tipos de dados
+    
+    //Declarar uma variável === declaração or declaration
+        //var _name,
+
+    //assignement ou declaração de valores
+     _name = "Myke"
+
+    //que tipo de dado foi declarado na variável: String
+    //console.log(typeof _name)
+
+   //como agrupar as seguintes declarações:
+   // let age = 20
+   //let isHuman = true
+
+    let age, isHuman
+
+    age = 18
+    isHuman = true
+
+   //como depurar multiplos argumentos em uma função
+    //console.log(_name, age, isHuman )
+
+   //escrita de texto + variáveis com contatenação
+    //console.log('O ' + _name + ' tem ' + age + ' anos.')
+    //console.log('o ' + _name + ' é humano?', isHuman)
+
+   //escrita de texto + variáveis com interpolação (template literals or template strings)
+
+    console.log(`O ${_name} tem ${age} anos, mas ele é humano? ${isHuman}`)
