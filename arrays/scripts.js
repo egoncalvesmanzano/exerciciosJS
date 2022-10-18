30a9cbd216ce2f45404f9e69002e9d38f99c1606
Eduardo Mauricio Gonçalves da Silva Manzano <e.goncalvesmanzano@gmail.com>
2022-10-18 14:50:54 -0300

    #alt

--- arrays/scripts.js
+++ arrays/scripts.js
@@ -1,9 +1,62 @@
-//Como declarar Arrays
+// como declarar arrays
 
-const animals [
-  'dog',
-  'cat',
-  'lion', 
+const animals = [
+    'lion',
+    'cat',
+    'dog',
+ ]
 
+ //acessar valores dentro da array
 
-]
+/*
+Note que na array a contagem começa em 0:
+
+    Array(3) [ "lion", "cat", "dog" ]
+​
+    0: "lion"
+    1: "cat"
+    2: "dog"​
+
+*/
+
+ console.log(animals[2])
+
+ /*
+  console.log(animals[2])
+    resultado: dog
+*/
+
+ /*
+Para saber quantidade dos elementos do array use o .lenght:
+
+  console.log(animals.length)
+    resultado: 3
+*/
+
+//posso colocar objetos dentro de uma array:
+
+const animalsTwo = [
+    'bird',
+    'rat',
+    {
+        name: "John",
+        age: "32"
+    }
+ ]
+
+ console.log(animalsTwo[2])
+
+ //posso chamar uma propriedade de um objeto que está dentro de um array colocando "".nomedapropriedade" depois dos colchetes "[ ]":
+ //Esta não é uma prática usual.
+
+const animalsThree = [
+    'bird',
+    'rat',
+    {
+        name: "John",
+        age: "32"
+    }
+ ]
+
+ console.log(animalsThree[2].name)
+
